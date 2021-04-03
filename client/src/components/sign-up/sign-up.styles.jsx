e5ce7270a6d5fb2b9e06fb860ec8fb36ab5c09a2
Yuvrajjain-001 <55662413+Yuvrajjain-001@users.noreply.github.com>
2021-04-03 15:37:17 +0530

    Update sign-up.styles.jsx

--- client/src/components/sign-up/sign-up.styles.jsx
+++ client/src/components/sign-up/sign-up.styles.jsx
@@ -4,8 +4,13 @@ export const SignUpContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 380px;
+
+  @media screen and (max-width: 800px) {
+    width: 90vw;
+  }
 `;
 
 export const SignUpTitle = styled.h2`
   margin: 10px 0;
+
 `;
